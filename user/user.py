146cd859194accb0b4635276cb247bbda4e1ad4c
Guillaume Acamer <guillaumeacamer@MacBook-Air-de-Guillaume.local>
2023-09-26 16:38:18 +0200

    debut point d'entree user

--- user/user.py
+++ user/user.py
@@ -15,6 +15,40 @@ with open('{}/databases/users.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
 
+@app.route("/users/<userid>", methods=['GET'])
+def get_user_byid(userid):
+    for user in users:
+        if str(user["id"]) == str(userid):
+            res = make_response(jsonify(user),200)
+            return res
+    return make_response(jsonify({"error":"User ID not found"}),400)
+
+@app.route("/usersbytitle", methods=['GET'])
+def get_user_byname():
+    json = ""
+    if request.args:
+        req = request.args
+        for user in users:
+            if str(user["name"]) == str(req["name"]):
+                json = user
+
+    if not json:
+        res = make_response(jsonify({"error":"user name not found"}),400)
+    else:
+        res = make_response(jsonify(json),200)
+    return res
+
+@app.route("/users/<userid>", methods=['POST'])
+def create_user(userid):
+    req = request.get_json()
+
+    for user in users:
+        if str(user["id"]) == str(userid):
+            return make_response(jsonify({"error":"user ID already exists"}),409)
+
+    users.append(req)
+    res = make_response(jsonify({"message":"user added"}),200)
+    return res
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
